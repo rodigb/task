57d66d8d1b5d8fc8c2fb59e0e07a9c6c68baedfa
rodigb <rodix08@gmail.com>
2022-07-17 17:47:36 +0100

    optimized drag&drop + loading feature added

--- src/pages/screen1.js
+++ src/pages/screen1.js
@@ -4,111 +4,100 @@ import { useState } from "react";
 import {useDropzone} from 'react-dropzone'
 import Dropzone from 'react-dropzone'
 import resultsTable from '../components/table';
+import { CircularProgress } from '@mui/material';
 
 var nr = ""
 var nrArray= []
 var aperture = ""
 var focusDistance = ""
 var textDetails = ""
+var finalValues;
 
 
 function Screen1() {
 
-  const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone();
 
-  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+  const [isLoading, setIsLoading] = useState(false);
 
-   
-  acceptedFiles.forEach((file) => {
-  const reader = new FileReader();
+  const onDrop = useCallback(acceptedFiles =>{
+    setIsLoading(true)
+    
+    acceptedFiles.forEach((file) => {
 
-  reader.onload = () => {
-  // Do whatever you want with the file contents
+      var result = []
+      var obj={}
+    
 
+      const reader = new FileReader()
+      
+      reader.onload = () => {
 
-    var textFile= reader.result
-    var lines = textFile.split("\n");
-    var result = []
-    var headers=lines[0].split(",");
-    var obj={}
+        var textFile= reader.result
+        var lines = textFile.split("\n");
+        var headers=lines[0].split(",");
 
+        for(var i=1;i<lines.length;i++){
+          var currentline=lines[i].split(":");
+          for(var j=0;j<headers.length;j++){
+            obj[currentline[j]] = currentline[1];
+            var newObject= obj
+          }
+        }
 
+        result.push(newObject)
 
-    for(var i=1;i<lines.length;i++){
+        result = JSON.stringify(result)
+        result = result.replace(/(?:\\[rn])+/g, "");
+        result = JSON.parse(result)
 
-      var currentline=lines[i].split(":");
+        var finalJSON = JSON.stringify(result);
+        var parsed = JSON.parse(finalJSON)
+        nr = file.name
+        nr = nr.split(".")
+        nr = nr[1]
+        aperture = parsed[0]["aperture (type float)"]
+        focusDistance = parsed[0]["focus (type float)"]
 
+        finalValues=nr+aperture+focusDistance
 
-      for(var j=0;j<headers.length;j++){
-        obj[currentline[j]] = currentline[1];
-        var newObject= obj
+        console.log(nr + aperture + focusDistance)
+
+        setIsLoading(false)
+
+        
+
       }
 
-    }
-    result.push(newObject)
+      reader.readAsText(file)
+
+    })
 
 
-      result = JSON.stringify(result)
-      result = result.replace(/(?:\\[rn])+/g, "");
-      result = JSON.parse(result)
+  })
 
-      var result2 = result.map(function(o1){
-        var o2 = {};
-        for(var key in o1){
-          o2[key.replace(/\d+/g,'')] = o1[key];
-        }
-        return o2;
-      });
-      var finalJSON = JSON.stringify(result2);
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
 
-      var parsed = JSON.parse(finalJSON)
-
-      nr = file.name
-      nr = nr.split(".")
-      nr = nr[1]
-      aperture = parsed[0]["aperture (type float)"]
-      focusDistance = parsed[0]["focus (type float)"]
-
-      loadingData()
-  }
+ 
 
 
 
-  reader.readAsText(acceptedFiles[0])
-
-})
-
-function loadingData(){
-
-  if(nr!=null && aperture!=null && focusDistance!=null){
-
-  console.log(nr + aperture + focusDistance)
-  return(<header>akjdfoksdnkjgasnhkjdshnagskjzhnsdkj</header>)
-
-  }
-
-}
-
+if(finalValues==null){
   return (
     <div className="screen1">
         <div {...getRootProps({ className: "dropzone" })}>
 
         <input id="fileItem" type="file1" className="input-zone" {...getInputProps()} />
-        {isDragActive ? (
-          <p className="dropzone-content">
-            Release to drop the files here
-          </p>
-        ) : (
-          <p className="dropzone-content">
-            Drag and drop the files of a sequence to extract the metadata
-          </p>
-        )}
+
+        {isLoading ? <CircularProgress size={100}/> :
+         (<p>Drag and drop the files of a sequence to extract the metadata</p>)}
+
 
         </div>
 
 
     </div>
   )
+}
 }
 
 
