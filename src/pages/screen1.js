7b73bb96a574963ee6c87f2f343101cb85c954f4
rodigb <rodix08@gmail.com>
2022-07-17 15:01:51 +0100

    single file uplaod with json

--- src/pages/screen1.js
+++ src/pages/screen1.js
@@ -7,37 +7,76 @@ import resultsTable from '../components/table';
 
 var nr = ""
 var nrArray= []
-var apeture = ""
+var aperture = ""
 var focusDistance = ""
 var textDetails = ""
 
 
 function Screen1() {
 
-  
-
   const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone();
 
 
-  
+  acceptedFiles.forEach((file) => {
+  const reader = new FileReader();
 
-  const files = acceptedFiles.map(file => (
+  reader.onload = () => {
+  // Do whatever you want with the file contents
+
+
+    var textFile= reader.result
+    var lines = textFile.split("\n");
+    var result = []
+    var headers=lines[0].split(",");
+    var obj={}
 
 
 
-    nr = file.name,
-    nr = nr.split("."),
-    nr = nr[1],
-    console.log(nr)
+    for(var i=1;i<lines.length;i++){
 
-  ));
+      var currentline=lines[i].split(":");
 
+
+      for(var j=0;j<headers.length;j++){
+        obj[currentline[j]] = currentline[1];
+        var newObject= obj
+      }
+
+    }
+    result.push(newObject)
+
+
+      result = JSON.stringify(result)
+      result = result.replace(/(?:\\[rn])+/g, "");
+      result = JSON.parse(result)
+
+      var result2 = result.map(function(o1){
+        var o2 = {};
+        for(var key in o1){
+          o2[key.replace(/\d+/g,'')] = o1[key];
+        }
+        return o2;
+      });
+      var finalJSON = JSON.stringify(result2);
+
+      var parsed = JSON.parse(finalJSON)
+
+      nr = file.name
+      nr = nr.split(".")
+      nr = nr[1]
+      aperture = parsed[0]["aperture (type float)"]
+      focusDistance = parsed[0]["focus (type float)"]
+
+      loadingData()
+  }
+
+
+
+  reader.readAsText(acceptedFiles[0])
+
+})
 
  
-
-
-
-
 
   return (
     <div className="screen1">
@@ -53,8 +92,9 @@ function Screen1() {
             Drag and drop the files of a sequence to extract the metadata
           </p>
         )}
-
+        
         </div>
+        
 
 
     </div>
